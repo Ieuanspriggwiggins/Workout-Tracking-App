ab9d625663711f9df3ebb60da498682d5f0b2dee
ieuan sprigg-wiggins <ieuanspriggwiggins@gmail.com>
2023-12-30 14:24:51 +0100

    Add lazy list for displaying the workout on the homepage.

--- app/src/main/java/com/ieuan/dev/yourworkouts/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ieuan/dev/yourworkouts/ui/home/HomeScreen.kt
@@ -1,8 +1,15 @@
 package com.ieuan.dev.yourworkouts.ui.home
 
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.ieuan.dev.yourworkouts.model.HomeScreenViewModel
@@ -12,6 +19,7 @@ import java.util.Calendar
 import java.util.Locale
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ieuan.dev.yourworkouts.R
+import com.ieuan.dev.yourworkouts.datasource.Exercise
 
 @Composable
 fun HomeScreen(
@@ -25,8 +33,20 @@ fun HomeScreen(
         navController = navController,
         screenTitle = stringResource(R.string.home_screen_title) + viewModel.currentDayString
     ) {
-        exerciseList.forEach{exercise ->
-            //TODO: Make exercises show on the homepage
+        LazyColumn(
+        ){
+            items(exerciseList) {exercise ->
+                ExerciseHomeScreenCard(exercise = exercise)
+            }
         }
+    }
+}
+
+@Composable
+fun ExerciseHomeScreenCard(exercise: Exercise) {
+    if(exercise.isDropSet){
+
+    }else{
+
     }
 }
